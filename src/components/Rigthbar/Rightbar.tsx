6b249bb89e9ab05ebc62d349e17d7683619b4065
Aleksandr <ryan00@mail.ru>
2023-08-15 20:17:25 +0300

    second commit

--- src/components/Rigthbar/Rightbar.tsx
+++ src/components/Rigthbar/Rightbar.tsx
@@ -1,4 +1,18 @@
-import rigthbar from "./rightbar.module.css";
+import style from "./rightbar.module.css";
 export const Rightbar: React.FC = () => {
-  return <div className={rigthbar.rightbar__wrapper}>Rightbar</div>;
+  return (
+    <div className={style.rightbar__wrapper}>
+      <div className={style.rightbar__container}>
+        <div className={style.rightbar__basket}>
+          <div className={style["rightbar__basket-text"]}>
+            <span>Корзина</span>
+            <span>2 остероида</span>
+          </div>
+          <button className={style["rightbar__basket-btn"]}>
+            <span>Отправить</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
